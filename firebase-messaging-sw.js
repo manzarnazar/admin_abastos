7298f343878d918fab8b0a6209e8d35cb15b43b4
manzarnazar <manzarnazar@gmail.com>
2026-02-04 18:09:01 +0000

    changes

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyAxAdlAJ5DB0CG-257qBUdaOzgJj2M3T1M",
+    authDomain: "abastos-app.firebaseapp.com",
+    projectId: "abastos-app",
+    storageBucket: "abastos-app.firebasestorage.app",
+    messagingSenderId: "329776474866",
+    appId: "1:329776474866:web:2d0a09f52ed1eda4c83e6b",
     measurementId: ""
 });
 
